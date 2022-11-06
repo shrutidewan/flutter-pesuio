b04daf545c30905973c360c6e08df1f12a11edf0
Shruti Dewan <shrutidewanaug@gmail.com>
2022-11-06 15:51:45 +0530

    flutter day 3

--- lib/main.dart
+++ lib/main.dart
@@ -70,46 +70,60 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'PESU',
+              style: TextStyle(fontSize: 36.0, fontFamily: 'Inter', color: Colors.blue, ),
+            ),
+            Image.asset(
+              'assets/images/cat.jpg',
+              height: 200.0,
+              width: 200.0,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+                'Janani',
+                style: TextStyle(
+                  fontSize: 26.0,
+                  color: Color(0xff80489C),
+                  fontFamily: 'Inter',
+                )
+
+            ),
+            Text(
+                'PES2UG21CS455',
+                style: TextStyle(
+                  fontSize: 26.0,
+                  color: Color(0xff80489C),
+                  fontFamily: 'Inter',
+                )
+            ),
+            Text(
+                '9972355302',
+                style: TextStyle(
+                  fontSize: 26.0,
+                  color: Color(0xff80489C),
+                  fontFamily: 'Inter',
+                )
+            ),
+            ElevatedButton(
+              onPressed: (){
+                print('Present');
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text('Present'),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
