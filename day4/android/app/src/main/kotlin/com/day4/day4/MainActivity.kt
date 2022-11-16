f4a53ac8018f8dd803b2371826367a314643530a
Shruti Dewan <90696645+shrutidewan@users.noreply.github.com>
2022-11-16 09:39:13 +0530

    update day4 code

---
+++ day4/android/app/src/main/kotlin/com/day4/day4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.day4.day4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ day4/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:day4/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
